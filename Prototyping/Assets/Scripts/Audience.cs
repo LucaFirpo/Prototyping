a2464f9b90b7d66e21059955426dc8bf5019870b
Luca Firpo <lucavfirpo@gmail.com>
2021-01-05 19:16:40 +0100

    Lancio implementato

---
+++ Prototyping/Assets/Scripts/Audience.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audience : MonoBehaviour
+{
+    public Thrower LeftTomato;
+    public Thrower CenterTomato;
+    public Thrower RightTomato;
+
+    public void Update()
+    {
+        LeftTomato.Throw();
+        CenterTomato.Throw();
+        RightTomato.Throw();
+    }
+}
---
+++ Prototyping/Assets/Scripts/Thrower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    public Transform Start;
+
+    public Transform Destination;
+
+    public float Speed;
+
+    private void Update()
+    {
+        if (transform.position == Destination.position)
+            transform.position = Start.position;
+    }
+
+    public void Throw()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Destination.position, Speed*Time.deltaTime);
+    }
+}
