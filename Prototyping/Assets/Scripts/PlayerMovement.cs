b40735683fec8d58aa5f1240ee93e1570b1d2e52
Luca Firpo <lucavfirpo@gmail.com>
2021-01-05 18:30:32 +0100

    Implemented movement

---
+++ Prototyping/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Distanza = 10;
+    
+    public Vector3 partenza;
+
+    private void Start()
+    {
+        partenza = this.transform.position;
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = new Vector3(partenza.x + Distanza, partenza.y, partenza.z);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = new Vector3(partenza.x - Distanza, partenza.y, partenza.z);
+        }
+        else
+        {
+            this.transform.position = partenza;
+        }
+    }
+}
